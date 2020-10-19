fd83b7faec7cb4d61b8775747efef13c1bd2bd04
Weslley Gomes Dantas <30627516+wslgs@users.noreply.github.com>
2020-10-19 17:27:27 -0300

    Create frame_buffer.h

---
+++ CG_atividade_2/frame_buffer.h
@@ -0,0 +1,8 @@
+#ifndef FRAME_BUFFER_H
+#define FRAME_BUFFER_H
+
+// Ponteiro para o primeiro byte do color buffer.
+unsigned char* fb_ptr;
+unsigned char* flipped_buff_ptr; //  flipped buffer
+
+#endif  // FRAME_BUFFER_H
