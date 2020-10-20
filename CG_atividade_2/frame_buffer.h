96fd9908d55f0b3852234405dcd40e6bc93c379f
Weslley Gomes Dantas <30627516+wslgs@users.noreply.github.com>
2020-10-20 11:17:26 -0300

    Delete frame_buffer.h

--- CG_atividade_2/frame_buffer.h
+++
@@ -1,8 +0,0 @@
-#ifndef FRAME_BUFFER_H
-#define FRAME_BUFFER_H
-
-// Ponteiro para o primeiro byte do color buffer.
-unsigned char* fb_ptr;
-unsigned char* flipped_buff_ptr; //  flipped buffer
-
-#endif  // FRAME_BUFFER_H
